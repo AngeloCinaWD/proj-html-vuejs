0df6b1d4ebb8d8fd1ee59b183b1087ba2044f5eb
Angelo <angelo.cina@yahoo.it>
2020-12-11 10:07:45 +0100

    Header vuejs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var app = new Vue ({ //instanzio vue
   el: "#app",
   data: {
-
-  },
-  methods: {
-    
+    logoHeader: "img/img/logo-sidearea-1.png",
+    headerMenu: "img/svg/svg-1.svg",
+    classe: "size13",
+    navBar: ["HOME", "PAGES", "PORTFOLIO", "BLOG", "SHOP", "ELEMENTS"]
   }
 });
